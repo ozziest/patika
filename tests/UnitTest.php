6e5a5470f46bd22cbd9397dde1d889b4c22e5880
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2015-09-12 18:27:45 +0300

    Unit tests were updated

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -1,14 +1,8 @@
 <?php 
 
 use Ozziest\Patika\Manager;
-use \Mockery as m;
 
 class UnitTest extends PHPUnit_Framework_TestCase {
-
-    public function tearDown()
-    {
-        m::close();
-    }
 
     /**
      * @expectedException \InvalidArgumentException
@@ -19,74 +13,58 @@ class UnitTest extends PHPUnit_Framework_TestCase {
         $patika = new Manager([]);
     }
 
-    public function testInit()
-    {
-        $patika = new Manager(['app' => 'App\Controllers']);
-        $request = $patika->getRequest();
-        $this->assertInstanceOf('\Ozziest\Patika\Request', $request);
-        $this->assertEquals('App\Controllers', $request->getMainNamespace());
-        $this->assertEquals('users/all', $request->getUrl());
-        $this->assertEquals("App\Controllers\Users", $request->getFullNamespace());
-        $this->assertEquals("all", $request->getAction());
-    }
-
+    /**
+     * @dataProvider urlProvider
+     */
     public function testLongUrl()
     {
         $_SERVER['REQUEST_URI'] = 'admin/manage/internal/users/all';
         $patika = new Manager(['app' => 'App\Controllers']);
         $request = $patika->getRequest();
+        $this->assertInstanceOf('\Ozziest\Patika\Request', $request);
         $this->assertEquals('App\Controllers', $request->getMainNamespace());
         $this->assertEquals('admin/manage/internal/users/all', $request->getUrl());
         $this->assertEquals("App\Controllers\Admin\Manage\Internal\Users", $request->getFullNamespace());
         $this->assertEquals("all", $request->getAction());
     }
 
-    public function testSimpleArgument()
-    {
-        $_SERVER['REQUEST_URI'] = 'users/get/1/2/3/foo/bar';
-        $patika = new Manager(['app' => 'App\Controllers']);
-        $request = $patika->getRequest();
-        $this->assertEquals('App\Controllers', $request->getMainNamespace());
-        $this->assertEquals('users/get/1/2/3/foo/bar', $request->getUrl());
-        $this->assertEquals("App\Controllers\Users", $request->getFullNamespace());
-        $this->assertEquals("get", $request->getAction());
-        $arguments = $request->getArguments();
-        $this->assertCount(5, $arguments);
-        $this->assertEquals("foo", $arguments[3]);
-    }
-
     /**
+     * @dataProvider urlProvider
      * @expectedException \Ozziest\Patika\Exceptions\ControllerNotFoundException
      */
-    public function testControllerNotFound()
+    public function testControllerNotFound($url, $controller, $method, $arguments)
     {
-        $_SERVER['REQUEST_URI'] = 'users/all';
+        $_SERVER['REQUEST_URI'] = $url;
         $patika = new Manager(['app' => 'App\Controllers']);
         $patika->call();
         $this->assertTrue(true);
     }
 
     /**
+     * @dataProvider urlProvider
      * @expectedException \Ozziest\Patika\Exceptions\MethodNotFoundException
      */
-    public function testMethodNotFound()
+    public function testMethodNotFound($url, $controller)
     {
-        $userController = $this->getMockBuilder('\App\Controllers\Users')->getMock();
-        $_SERVER['REQUEST_URI'] = 'users/all';
+        $_SERVER['REQUEST_URI'] = $url;
+        $userController = $this->getMockBuilder($controller)->getMock();
         $patika = new Manager(['app' => 'App\Controllers']);
         $patika->call();
         $this->assertTrue(true);
     }
 
-    public function testSuccess()
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testSuccess($url, $controller, $method, $arguments)
     {
-        $userController = $this->getMockBuilder('\App\Controllers\Users')
-            ->setMethods(array('all'))
+        $_SERVER['REQUEST_URI'] = $url;
+        $userController = $this->getMockBuilder($controller)
+            ->setMethods(array($method))
             ->getMock();
         $userController->expects($this->once())
-             ->method('all')
+             ->method($method)
              ->with();
-        $_SERVER['REQUEST_URI'] = 'users/all';
         $patika = new Manager(['app' => 'App\Controllers']);
         $patika->setMock($userController);
         $patika->call(array('constructer_data' => true));
@@ -94,7 +72,7 @@ class UnitTest extends PHPUnit_Framework_TestCase {
     }
 
     /**
-     * @dataProvider sampleUrlProvider
+     * @dataProvider urlProvider
      */
     public function testArguments($url, $controller, $method, $arguments)
     {
@@ -110,9 +88,10 @@ class UnitTest extends PHPUnit_Framework_TestCase {
         $patika->call(array('constructer_data' => true));
     }
 
-    public function sampleUrlProvider()
+    public function urlProvider()
     {
         return array(
+          array('users/get', '\App\Controllers\Users', 'get', []),
           array('users/get', '\App\Controllers\Users', 'get', []),
           array('users/manage/get', '\App\Controllers\Users\Manage', 'get', []),
           array('users/get/1/2/foo/bar', '\App\Controllers\Users', 'get', ['1', '2', 'foo', 'bar']),
